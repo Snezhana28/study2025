38b8e2b18677f7065b57322822a425f507001180
Snezhana28 <snezhana.dmitrieva.96@mail.ru>
2025-10-20 00:36:52 +0300

    implementation of the second hw

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sergeidovydenko/num2word"
+)
 
 func main() {
-	fmt.Println("hello world")
+	var a int
+	n, err := fmt.Scan(&a)
+	if err != nil || n == 0 {
+		fmt.Println("Ошибка ввода: введите целое число")
+		return
+	}
+	if a >= 12307 {
+		fmt.Printf("Введенное число %d должно быть меньше 12307\n", a)
+		return
+	}
+	if a < 0 {
+		a = -a
+	}
+
+	if a%13 == 0 && a%9 == 0 {
+		fmt.Println("service error")
+		return
+	}
+	switch {
+	case a < 0:
+
+		a = a * -1
+	case a%7 == 0:
+
+		a = a * 39
+	case a%9 == 0:
+		a = a*13 + 1
+	default:
+
+		a = (a + 2) * 3
+	}
+	a = a + 1
+
+	str := num2word.Convert(a, false)
+
+	fmt.Printf("Итоговое число: %d\n", a)
+
+	fmt.Printf("Итоговое число прописью: %s\n", str)
+
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Snezhana28/study2025
+
+go 1.24.3
+
+require github.com/sergeidovydenko/num2word v0.0.0-20190114112741-8a9b630938e5 // indirect
